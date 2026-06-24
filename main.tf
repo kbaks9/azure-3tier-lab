da96d3a9eecedcf60bb3ae88e5870f5d79e810a5
kbaks9 <kamaal.bakar@outlook.com>
2026-06-24 19:26:06 +0100

    Added the network security group module & updated the network to make sure nsg attach to the subnet

--- main.tf
+++ main.tf
@@ -15,11 +15,24 @@ module "network" {
   prefix_web         = var.prefix_web
   prefix_app         = var.prefix_app
   prefix_data        = var.prefix_data
+  security_group     = path.module.nsg.nsg_name.id
 }
 
-# module "nsg" {
-
-# }
+module "nsg" {
+  source                          = "./modules/nsg"
+  resource_group                  = var.resource_group
+  location                        = var.location
+  nsg_name                        = var.nsg_name
+  rule_name                       = var.rule_name
+  rule_priority                   = var.rule_priority
+  rule_direction                  = var.rule_direction
+  rule_access                     = var.rule_access
+  rule_protocol                   = var.rule_protocol
+  rule_source_port_range          = var.rule_source_port_range
+  rule_destination_port_range     = var.rule_destination_port_range
+  rule_source_address_prefix      = var.rule_source_address_prefix
+  rule_destination_address_prefix = var.rule_destination_address_prefix
+}
 
 # module "vm" {
 
