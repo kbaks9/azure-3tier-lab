457b4ec6105199ed71366da2217de36ced88f28b
kbaks9 <kamaal.bakar@outlook.com>
2026-06-24 21:18:34 +0100

    Created security rules for all 3 subnets

--- main.tf
+++ main.tf
@@ -15,23 +15,83 @@ module "network" {
   prefix_web         = var.prefix_web
   prefix_app         = var.prefix_app
   prefix_data        = var.prefix_data
-  security_group     = path.module.nsg.nsg_name.id
+  security_group     = module.nsg.nsg_id
 }
 
 module "nsg" {
-  source                          = "./modules/nsg"
-  resource_group                  = var.resource_group
-  location                        = var.location
-  nsg_name                        = var.nsg_name
-  rule_name                       = var.rule_name
-  rule_priority                   = var.rule_priority
-  rule_direction                  = var.rule_direction
-  rule_access                     = var.rule_access
-  rule_protocol                   = var.rule_protocol
-  rule_source_port_range          = var.rule_source_port_range
-  rule_destination_port_range     = var.rule_destination_port_range
-  rule_source_address_prefix      = var.rule_source_address_prefix
-  rule_destination_address_prefix = var.rule_destination_address_prefix
+  source         = "./modules/nsg"
+  resource_group = azurerm_resource_group.rg-grp.name
+  location       = azurerm_resource_group.rg-grp.location
+  nsg_web_name   = var.nsg_web_name
+  nsg_app_name   = var.nsg_app_name
+  nsg_data_name  = var.nsg_data_name
+  rules = [
+    {
+      name                       = "Allow_Admin_SSH"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = var.my_ip
+      destination_address_prefix = "*"
+    },
+
+    {
+
+      name                       = "Web_Allow_HTTP" # Web Subnet Allows HTTP
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "Internet"
+      destination_address_prefix = "10.0.1.0/24"
+    },
+
+    {
+      name                       = "Web_Allow_HTTPS" # Web Subnet Allows HTTPS
+      priority                   = 120
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "Internet"
+      destination_address_prefix = "10.0.1.0/24"
+    },
+
+    {
+      name                       = "Allow_Web_To_App" # For Web -> App subnet
+      priority                   = 130
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8080"
+      source_address_prefix      = "10.0.1.0/24"
+      destination_address_prefix = "10.0.2.0/24"
+
+    },
+
+    {
+      name                       = "Allow_App_To_Data"
+      priority                   = 140
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "1433"
+      source_address_prefix      = "10.0.2.0/24"
+      destination_address_prefix = "10.0.3.0/24"
+  }]
+
+
+  depends_on = [
+    azurerm_resource_group.rg-grp
+  ]
 }
 
 # module "vm" {
