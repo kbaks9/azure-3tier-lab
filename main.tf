8ea705ae415d74e726bf10543866f48dc4820c3a
kbaks9 <kamaal.bakar@outlook.com>
2026-06-25 17:19:48 +0100

    Fully configured and automated infrastructure deployment of subets, vms, nsgs, public ips, nics

--- main.tf
+++ main.tf
@@ -28,7 +28,8 @@ module "nsg_web" {
   resource_group = azurerm_resource_group.rg-grp.name
   location       = azurerm_resource_group.rg-grp.location
 
-  nsg_name = var.nsg_web_name
+  nsg_name  = var.nsg_web_name
+  subnet_id = module.network.subnet_web_id
   rules = [
     {
       name                       = "Allow_Admin_SSH"
@@ -56,15 +57,17 @@ module "nsg_web" {
     },
 
     {
-      name                       = "Web_Allow_HTTPS" # Web Subnet Allows HTTPS
-      priority                   = 120
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "443"
-      source_address_prefix      = "Internet"
-      destination_address_prefix = "10.0.1.0/24"
+      name                   = "Web_Allow_HTTPS" # Web Subnet Allows HTTPS
+      priority               = 120
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "Tcp"
+      source_port_range      = "*"
+      destination_port_range = "443"
+      # source_address_prefix      = "Internet"
+      # destination_address_prefix = "10.0.1.0/24"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
   }]
 }
 
@@ -73,17 +76,20 @@ module "nsg_app" {
   resource_group = azurerm_resource_group.rg-grp.name
   location       = azurerm_resource_group.rg-grp.location
   nsg_name       = var.nsg_app_name
+  subnet_id      = module.network.subnet_app_id
   rules = [
     {
-      name                       = "Allow_Web_To_App" # For Web -> App subnet
-      priority                   = 130
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "8080"
-      source_address_prefix      = "10.0.1.0/24"
-      destination_address_prefix = "10.0.2.0/24"
+      name                   = "Allow_Web_To_App" # For Web -> App subnet
+      priority               = 130
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "Tcp"
+      source_port_range      = "*"
+      destination_port_range = "8080"
+      # source_address_prefix      = "10.0.1.0/24"
+      # destination_address_prefix = "10.0.2.0/24"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
 
   }]
 }
@@ -93,53 +99,26 @@ module "nsg_data" {
   resource_group = azurerm_resource_group.rg-grp.name
   location       = azurerm_resource_group.rg-grp.location
   nsg_name       = var.nsg_data_name
+  subnet_id      = module.network.subnet_data_id
   rules = [
     {
-      name                       = "Allow_App_To_Data" # Allow App traffic -> Data
-      priority                   = 140
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "1433"
-      source_address_prefix      = "10.0.2.0/24"
-      destination_address_prefix = "10.0.3.0/24"
+      name                   = "Allow_App_To_Data" # Allow App traffic -> Data
+      priority               = 140
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "Tcp"
+      source_port_range      = "*"
+      destination_port_range = "1433"
+      # source_address_prefix      = "10.0.2.0/24"
+      # destination_address_prefix = "10.0.3.0/24"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
   }]
 }
 
-# locals {
-#   machines = {
-#     web01  = module.network.subnet_web_id
-#     web02  = module.network.subnet_web_id
-#     app01  = module.network.subnet_app_id
-#     app02  = module.network.subnet_app_id
-#     data01 = module.network.subnet_data_id
-#     data02 = module.network.subnet_data_id
-#   }
-# }
-
-# module "vm" {
-#   source         = "./modules/vm"
-#   resource_group = azurerm_resource_group.rg-grp.name
-#   location       = azurerm_resource_group.rg-grp.location
-#   for_each       = local.machines
-#   vm_name        = "vm-${each.key}"
-#   nic_name       = "nic-${each.key}"
-#   subnet_id      = each.value
-# }
-
-module "web_vms" {
-  source         = "./modules/vm"
-  resource_group = azurerm_resource_group.rg-grp.name
-  location       = azurerm_resource_group.rg-grp.location
-  for_each       = toset(["web01", "web02"])
-
-  nic_name  = "nic-${each.key}"
-  vm_name   = "vm-${each.key}"
-  subnet_id = module.network.subnet_web_id
+locals {
+  ssh_public_key = file(pathexpand("~/.ssh/id_ed25519.pub"))
 }
-
-# VS
 
 module "web_vms" {
   source         = "./modules/vm"
@@ -147,36 +126,58 @@ module "web_vms" {
   location       = azurerm_resource_group.rg-grp.location
 
   for_each = {
-    web01 = true
-    web02 = false
+    web01 = {
+      create_public_ip = true
+      admin_username   = "steve"
+      admin_password   = var.web01_admin_password
+      ssh_public_key   = local.ssh_public_key
+    }
+
+    web02 = {
+      create_public_ip = true
+      admin_username   = "tony"
+      admin_password   = var.web02_admin_password
+      ssh_public_key   = local.ssh_public_key
+    }
   }
 
-  nic_name  = "nic-${each.key}"
-  vm_name   = "vm-${each.key}"
-  subnet_id = module.network.subnet_web_id
-
-  create_public_ip = each.value
+  nic_name         = "nic-${each.key}"
+  vm_name          = "vm-${each.key}"
+  subnet_id        = module.network.subnet_web_id
+  create_public_ip = each.value.create_public_ip
+  admin_username   = each.value.admin_username
+  admin_password   = each.value.admin_password
+  ssh_public_key   = each.value.ssh_public_key
 }
 
 module "app_vms" {
   source         = "./modules/vm"
   resource_group = azurerm_resource_group.rg-grp.name
   location       = azurerm_resource_group.rg-grp.location
-  for_each       = toset(["app01", "app02"])
+
+  for_each = toset(["app01", "app02"])
 
   nic_name  = "nic-${each.key}"
   vm_name   = "vm-${each.key}"
   subnet_id = module.network.subnet_app_id
-}
 
+  create_public_ip = false
+  admin_username   = "azureadmin"
+  ssh_public_key   = local.ssh_public_key
+}
 
 module "data_vms" {
   source         = "./modules/vm"
   resource_group = azurerm_resource_group.rg-grp.name
   location       = azurerm_resource_group.rg-grp.location
-  for_each       = toset(["data01", "data02"])
+
+  for_each = toset(["data01", "data02"])
 
   nic_name  = "nic-${each.key}"
   vm_name   = "vm-${each.key}"
   subnet_id = module.network.subnet_data_id
+
+  create_public_ip = false
+  admin_username   = "azureadmin"
+  ssh_public_key   = local.ssh_public_key
 }
