5f22996239c6ee7520f5eb10540ca920fccff0f4
kbaks9 <kamaal.bakar@outlook.com>
2026-06-25 01:01:16 +0100

    added outputs for subnet ids for 3 subets

---
+++ modules/network/outputs.tf
@@ -0,0 +1,12 @@
+output "subnet_web_id" {
+  value = azurerm_subnet.web.id
+}
+
+output "subnet_app_id" {
+  value = azurerm_subnet.app.id
+}
+
+output "subnet_data_id" {
+  value = azurerm_subnet.data.id
+}
+
