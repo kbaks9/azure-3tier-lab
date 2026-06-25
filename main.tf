7bae3f844597548b7a6cdc1d7cf73d41a6ba3a29
kbaks9 <kamaal.bakar@outlook.com>
2026-06-25 17:40:44 +0100

    Limited source address prefix and destination for Nsg_data & nsg_app. It'll only allow private network addresses, no access to the internet

--- main.tf
+++ main.tf
@@ -57,17 +57,15 @@ module "nsg_web" {
     },
 
     {
-      name                   = "Web_Allow_HTTPS" # Web Subnet Allows HTTPS
-      priority               = 120
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "Tcp"
-      source_port_range      = "*"
-      destination_port_range = "443"
-      # source_address_prefix      = "Internet"
-      # destination_address_prefix = "10.0.1.0/24"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
+      name                       = "Web_Allow_HTTPS" # Web Subnet Allows HTTPS
+      priority                   = 120
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "Internet"
+      destination_address_prefix = "10.0.1.0/24"
   }]
 }
 
@@ -79,18 +77,15 @@ module "nsg_app" {
   subnet_id      = module.network.subnet_app_id
   rules = [
     {
-      name                   = "Allow_Web_To_App" # For Web -> App subnet
-      priority               = 130
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "Tcp"
-      source_port_range      = "*"
-      destination_port_range = "8080"
-      # source_address_prefix      = "10.0.1.0/24"
-      # destination_address_prefix = "10.0.2.0/24"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-
+      name                       = "Allow_Web_To_App" # For Web -> App subnet
+      priority                   = 130
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8080"
+      source_address_prefix      = "10.0.1.0/24"
+      destination_address_prefix = "10.0.2.0/24"
   }]
 }
 
@@ -102,17 +97,15 @@ module "nsg_data" {
   subnet_id      = module.network.subnet_data_id
   rules = [
     {
-      name                   = "Allow_App_To_Data" # Allow App traffic -> Data
-      priority               = 140
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "Tcp"
-      source_port_range      = "*"
-      destination_port_range = "1433"
-      # source_address_prefix      = "10.0.2.0/24"
-      # destination_address_prefix = "10.0.3.0/24"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
+      name                       = "Allow_App_To_Data" # Allow App traffic -> Data
+      priority                   = 140
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "1433"
+      source_address_prefix      = "10.0.2.0/24"
+      destination_address_prefix = "10.0.3.0/24"
   }]
 }
 
